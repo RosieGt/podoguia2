d75273157c11f48e0890fc3ce392126b456740a7
RosieGt <118468643+RosieGt@users.noreply.github.com>
2024-06-16 20:40:16 -0300

    Update settings.py

--- podoguia/settings.py
+++ podoguia/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
 }
 
 
- Password validation
- https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+# Password validation
+ # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
