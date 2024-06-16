5768bade5c9d460338b94974d4a898a336e26966
RosieGt <118468643+RosieGt@users.noreply.github.com>
2024-06-16 19:53:05 -0300

    Update settings.py

--- podoguia/settings.py
+++ podoguia/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-gpt4cg^fp-7tb+ii&fmnsqts00x0yzcqh!@%ek1*y&&scacp_0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wbmxgi.conteige.cloud']
+ALLOWED_HOSTS = ['podoguia.com']
 
 
 # Application definition
