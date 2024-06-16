95e091c0705259ef8b8c7ed06f9eb96cd9626333
RosieGt <118468643+RosieGt@users.noreply.github.com>
2024-06-16 20:19:19 -0300

    Update settings.py

--- podoguia/settings.py
+++ podoguia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-gpt4cg^fp-7tb+ii&fmnsqts00x0yzcqh!@%ek1*y&&scacp_0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['wbmxgi.conteige.cloud', '177.131.142.30']
 
