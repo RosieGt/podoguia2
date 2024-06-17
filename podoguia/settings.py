a1e99a508a0e4b23cde49d4635a71d30c847b8be
RosieGt <118468643+RosieGt@users.noreply.github.com>
2024-06-16 21:49:24 -0300

    Update settings.py

--- podoguia/settings.py
+++ podoguia/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['wbmxgi.conteige.cloud']
 
-CSRF_TRUSTED_ORIGINS = ['wbmxgi.conteige.cloud']
+CSRF_TRUSTED_ORIGINS = ['https://wbmxgi.conteige.cloud']
 
 
 # Application definition
