0ed1a2a845194bbb8079e3cdbc751ee7bbf9e0e7
RosieGt <118468643+RosieGt@users.noreply.github.com>
2024-08-20 21:48:01 -0300

    updatedados

--- podoguia/settings.py
+++ podoguia/settings.py
@@ -80,11 +80,7 @@ WSGI_APPLICATION = 'podoguia.wsgi.application'
  # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
-        
-    }
+   
 }
 
 
